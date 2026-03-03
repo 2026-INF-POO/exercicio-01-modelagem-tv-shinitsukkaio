1ce5e52f6dd28e4ae954c3318922daa3d49c3a28
shinitsukkaio <136288418+shinitsukkaio@users.noreply.github.com>
2026-03-03 09:05:06 -0300

    Add TV class with volume control methods
    Fix class definition and volume control methods.

--- TV.java
+++ TV.java
@@ -1,1 +1,22 @@
+class TV{
+  int tamanho;
+  int volume=5;
+  string marca; 
+  int voltagem;
+  int canal;
 
+  int aumentarvolume(){
+    if(volume <10){
+      volume++;
+    }
+    return volume;
+  }
+
+  int diminuirvolume(){
+    if(volume>10){
+      volume--;
+    }
+    return volume;
+  }
+  
+  
